43b7154fe4686dd55ff3d35f2176c8a93e4217d5
blinry <mail@blinry.org>
2022-07-30 15:16:11 +0200

    Use sqlite3 directly, to be able to run it on Android, maybe?

--- anki-social.py
+++ anki-social.py
@@ -1,4 +1,4 @@
-from anki.collection import Collection
+import sqlite3
 import datetime
 import glob
 import appdirs
@@ -16,7 +16,7 @@ if not path:
     print("No Anki collection found")
     exit(1)
 
-col = Collection(path[0])
+con = sqlite3.connect(path[0])
 
 def timestamp(dt):
     return int(dt.timestamp()*1000)
@@ -28,6 +28,9 @@ def section(name):
     print("\n" + name)
     print("="*len(name))
     print()
+
+def scalar(query, arguments=()):
+    return con.execute(query, arguments).fetchone()[0]
 
 achievements = []
 next_achievements = []
@@ -43,15 +46,15 @@ now = datetime.datetime.now()
 last_midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
 
 section("Totals")
-cards = col.db.scalar("select count() from cards")
+cards = scalar("select count() from cards")
 print("You have %d cards." % cards)
-reviews = col.db.scalar("select count() from revlog")
-first_review = col.db.scalar("select min(id) from revlog")
+reviews = scalar("select count() from revlog")
+first_review = scalar("select min(id) from revlog")
 days_since_first_review = (now - dt(first_review)).days
 reviews_per_day = reviews / days_since_first_review
 print("You did %d reviews since %s (that's %.2f reviews per day)." % (reviews, dt(first_review).strftime("%Y-%m-%d"), reviews_per_day))
 achievement_ladder("{} reviews in total", reviews, [10, 50, 100, 1000, 5000, 10000, 50000, 100000])
-seconds_spent = col.db.scalar("select sum(time)/1000 from revlog")
+seconds_spent = scalar("select sum(time)/1000 from revlog")
 minutes_spent = int(seconds_spent / 60)
 hours_spent = int(minutes_spent / 60)
 days_spent = int(hours_spent / 24)
@@ -75,8 +78,8 @@ for days_ago in range(365*10):
     from_time = last_midnight - datetime.timedelta(days=days_ago)
     to_time = last_midnight - datetime.timedelta(days=days_ago-1)
 
-    count = col.db.scalar("select count() from revlog where id > ? and id < ?",
-            timestamp(from_time), timestamp(to_time))
+    count = scalar("select count() from revlog where id > ? and id < ?",
+            (timestamp(from_time), timestamp(to_time)))
     if days_ago <= 14:
         print("On %s, you did %d reviews" % (from_time.strftime("%Y-%m-%d"), count))
     if count > 0:
@@ -96,8 +99,8 @@ for months_ago in range(1,12):
     from_time = last_midnight - datetime.timedelta(days=30*months_ago)
     to_time = last_midnight - datetime.timedelta(days=30*(months_ago-1))
 
-    created = col.db.scalar("select count() from cards where id > ? and id < ?",
-            timestamp(from_time), timestamp(to_time))
+    created = scalar("select count() from cards where id > ? and id < ?",
+            (timestamp(from_time), timestamp(to_time)))
 
     print("Between %s and %s, you created %d cards" % (from_time.strftime("%Y-%m-%d"), to_time.strftime("%Y-%m-%d"), created))
 
@@ -109,4 +112,4 @@ section("Upcoming achievements")
 for achievement in next_achievements:
     print(achievement)
 
-col.close()
+con.close()
