c4de023f0d4bcf706bd6072d1c8c7809068a23db
blinry <mail@blinry.org>
2022-08-03 18:12:20 +0200

    Refactor score calculations into their own classes

--- anki-social.py
+++ anki-social.py
@@ -4,8 +4,9 @@ import datetime
 import glob
 import appdirs
 import os
+import sys
 
-def find_anki_db():
+def find_db_path():
     path = None
     for appname in ("anki", "Anki2"):
         path = glob.glob(appdirs.user_config_dir(appname=appname) + "/**/collection.anki2")
@@ -22,106 +23,166 @@ def find_anki_db():
             path = None
     return path
 
-path = find_anki_db()
-if not path:
-    print("No Anki collection found")
-    exit(1)
-
-con = sqlite3.connect("file:"+path+"?immutable=1", uri=True)
-
 def timestamp(dt):
     return int(dt.timestamp()*1000)
 
 def dt(timestamp):
     return datetime.datetime.fromtimestamp(timestamp/1000)
 
-def section(name):
-    print("\n" + name)
-    print("="*len(name))
-    print()
+class Score:
+    def generate_achievements_until(self, db, until):
+        achievements = []
+        value = self.calculate(db, until)
+        for ladder_step in self.ladder:
+            if value >= ladder_step:
+                achievements.append(self.description.format(ladder_step))
+        return achievements
+    def upcoming(self, db, until):
+        value = self.calculate(db, until)
+        for ladder_step in self.ladder:
+            if value < ladder_step:
+                return "{}: {} (currently {})".format(self.name, ladder_step, value)
+        return None
 
-def scalar(query, arguments=()):
-    return con.execute(query, arguments).fetchone()[0]
+class SimpleScore(Score):
+    def __init__(self, name, description, query, ladder):
+        self.name = name
+        self.description = description
+        self.query = query
+        self.ladder = ladder
+    def calculate(self, db, up_to):
+        up_to_ts = timestamp(up_to)
+        return db.scalar(self.query, (up_to_ts,))
 
-achievements = []
-next_achievements = []
-def achievement_ladder(name, value, steps):
-    for step in steps:
-        if value >= step:
-            achievements.append(name.format(step))
-        else:
-            next_achievements.append(name.format(step) + " (currently {})".format(value))
-            break
+class StreakScore(Score):
+    def __init__(self, name, description, current, ladder):
+        self.name = name
+        self.description = description
+        self.current = current
+        self.ladder = ladder
+        self.diagram = ""
+    def calculate(self, db, up_to):
+        self.diagram = ""
 
-now = datetime.datetime.now()
-last_midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+        last_midnight = up_to.replace(hour=0, minute=0, second=0, microsecond=0)
 
-section("Totals")
-cards = scalar("select count() from cards")
-print("You have %d cards." % cards)
-reviews = scalar("select count() from revlog")
-first_review = scalar("select min(id) from revlog")
-days_since_first_review = (now - dt(first_review)).days
-reviews_per_day = reviews / days_since_first_review
-print("You did %d reviews since %s (that's %.2f reviews per day)." % (reviews, dt(first_review).strftime("%Y-%m-%d"), reviews_per_day))
-achievement_ladder("{} reviews in total", reviews, [10, 50, 100, 1000, 5000, 10000, 50000, 100000])
-seconds_spent = scalar("select sum(time)/1000 from revlog")
-minutes_spent = int(seconds_spent / 60)
-hours_spent = int(minutes_spent / 60)
-days_spent = int(hours_spent / 24)
-if days_spent > 1:
-    print("You spent %d days reviewing cards." % days_spent)
-elif hours_spent > 1:
-    print("You spent %d hours reviewing cards." % hours_spent)
-else:
-    print("You spent %d minutes reviewing cards." % minutes_spent)
-print("(That's %.2f minutes per day)." % (minutes_spent / days_since_first_review))
-achievement_ladder("{} minutes spent reviewing cards", minutes_spent, [10, 30])
-achievement_ladder("{} hours spent reviewing cards", hours_spent, [1, 3, 10])
-achievement_ladder("{} days spent reviewing cards", days_spent, [1, 3, 7, 14, 30])
-
-section("Reviews")
-streak = 0
-current_streak = 0
-max_streak = 0
-current = True
-for days_ago in range(365*10):
-    from_time = last_midnight - datetime.timedelta(days=days_ago)
-    to_time = last_midnight - datetime.timedelta(days=days_ago-1)
-
-    count = scalar("select count() from revlog where id > ? and id < ?",
-            (timestamp(from_time), timestamp(to_time)))
-    if days_ago <= 14:
-        print("On %s, you did %d reviews" % (from_time.strftime("%Y-%m-%d"), count))
-    if count > 0:
-        streak += 1
-    else:
-        if current:
-            current_streak = streak
-            current = False
-        max_streak = max(streak, max_streak)
+        best_streak = 0
         streak = 0
-print("\nYou are on a %d-day streak!" % current_streak)
-achievement_ladder("{}-day current streak", current_streak, [3, 7, 14, 30, 50, 75, 125, 180, 250, 365])
-print("Your best streak was a %d-day streak!" % max_streak)
-achievement_ladder("{}-day maximum streak", max_streak, [3, 7, 14, 30, 50, 75, 125, 180, 250, 365])
+        max_freeze_days = 2
+        freeze_days_left = max_freeze_days
+        for days_ago in range(10*365):
+            from_time = last_midnight - datetime.timedelta(days=days_ago)
+            to_time = last_midnight - datetime.timedelta(days=days_ago-1)
 
-section("Creations")
-for months_ago in range(1,12):
-    from_time = last_midnight - datetime.timedelta(days=30*months_ago)
-    to_time = last_midnight - datetime.timedelta(days=30*(months_ago-1))
+            count = db.scalar("select count() from revlog where id > ? and id < ?",
+                    (timestamp(from_time), timestamp(to_time)))
+            if count > 0:
+                streak += 1
+                self.diagram = "*" + self.diagram
+                if streak > best_streak:
+                    best_streak = streak
+                freeze_days_left = max_freeze_days
+            else:
+                if freeze_days_left > 0:
+                    freeze_days_left -= 1
+                    self.diagram = "f" + self.diagram
+                else:
+                    if self.current:
+                        self.diagram = "Streak diagram: " + self.diagram
+                        return streak
+                    else:
+                        streak = 0
+            if from_time.weekday() == 0:
+                self.diagram = "|" + self.diagram
 
-    created = scalar("select count() from cards where id > ? and id < ?",
-            (timestamp(from_time), timestamp(to_time)))
+        if self.current:
+            return streak
+        else:
+            self.diagram = ""
+            return best_streak
 
-    print("Between %s and %s, you created %d cards" % (from_time.strftime("%Y-%m-%d"), to_time.strftime("%Y-%m-%d"), created))
+class AnkiDB:
+    def __init__(self, path):
+        self.path = path
+        self.con = sqlite3.connect("file:"+path+"?immutable=1", uri=True)
+        self.scores = []
 
-section("Your achievements")
-for achievement in achievements:
-    print(achievement)
+    def __del__(self):
+        self.con.close()
 
-section("Upcoming achievements")
-for achievement in next_achievements:
-    print(achievement)
+    def add_score(self, score):
+        self.scores.append(score)
 
-con.close()
+    def scalar(self, query, arguments=()):
+        return self.con.execute(query, arguments).fetchone()[0]
+
+    def print_stats(self):
+        print()
+        for score in self.scores:
+            value = score.calculate(self, datetime.datetime.now())
+            if hasattr(score, "diagram") and score.diagram != "":
+                print(score.diagram)
+    def generate_achievements_since(self, until, since):
+        achievements_now = []
+        achievements_then = []
+        for score in self.scores:
+            achievements_now.extend(score.generate_achievements_until(self, until))
+            achievements_then.extend(score.generate_achievements_until(self, since))
+        new_achievements = list(set(achievements_now) - set(achievements_then))
+        return new_achievements
+    def upcoming_achievements(self, now):
+        upcoming = []
+        for score in self.scores:
+            upcoming.append(score.upcoming(self, now))
+        return upcoming
+
+path = find_db_path()
+
+if not path:
+    print("Anki database not found")
+    exit(1)
+
+db = AnkiDB(path)
+now = datetime.datetime.now()
+last_run_file = sys.path[0]+'/last_run'
+
+if os.path.exists(last_run_file):
+    with open(last_run_file, 'r') as f:
+        last_run = dt(int(f.read()))
+        print("Last run: {}".format(last_run))
+else:
+    print("Running script for the first time!")
+    last_run = now
+
+count_ladder = [10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
+time_ladder = [0.5]
+for hours in range(1,11):
+    time_ladder.append(hours)
+for hours in range(10,1001,10):
+    time_ladder.append(hours)
+streak_ladder = [3, 7, 14, 30, 50, 75, 125, 180, 250, 365, 500, 600, 800, 1000]
+
+db.add_score(SimpleScore("reviews", "reviewed {} cards", "select count() from revlog where id < ?", count_ladder))
+db.add_score(SimpleScore("cards added", "added {} cards", "select count() from cards where id < ?", count_ladder))
+db.add_score(SimpleScore("hours studied", "studied for {} hours", "select sum(time)/1000/60/60 from revlog where id < ?", time_ladder))
+db.add_score(StreakScore("current streak", "{}-day streak", True, streak_ladder))
+db.add_score(StreakScore("best streak", "new best streak of {} days", False, streak_ladder))
+
+db.print_stats()
+
+achievements = db.generate_achievements_since(now, last_run)
+
+if len(achievements) > 0:
+    print("\nNew achievements!\n")
+    for achievement in achievements:
+        print("- " + achievement)
+
+upcoming = db.upcoming_achievements(now)
+
+if len(upcoming) > 0:
+    print("\nUpcoming achievements:\n")
+    for achievement in upcoming:
+        print("- " + achievement)
+
+with open(last_run_file, 'w') as f:
+    f.write(str(timestamp(now)))
